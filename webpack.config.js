bdd4fc9116876c3aae54cbdafccb606a6e34e380
amepablo <amepablo@gmail.com>
2022-04-07 11:20:42 -0300

    config style.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -27,6 +29,14 @@ module.exports = {
                 inject: true,
                 template: './public/index.html',
                 filename: './index.html'
+            }
+        ),
+        new CopyPlugin(
+            {
+                patterns: [{
+                    from: './src/styles/styles.css',
+                    to: '' ,
+                }]
             }
         )
     ]
